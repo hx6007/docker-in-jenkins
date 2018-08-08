e999244a3425667b3dad640dea3c47df03ff1106
root <root@localhost.localdomain>
2018-08-08 13:40:34 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd centos7/
+
+docker build -t csphere/centos:7.1 .
+
+cd ../jdk
+
+docker build -t csphere/jdk:1.7.0 .
+
+cd ../jenkins/
+
+docker build -t csphere/jenkins:1.609 .
+
+docker run -d -p 8080:8080 --restart=always --name jenkins -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home:/var/jenkins_home -v /usr/lib64/libltdl.so.7:/usr/lib64/libltdl.so.7 -v /root/.ssh/:/root/.ssh csphere/jenkins:1.609
